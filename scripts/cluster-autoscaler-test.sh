bac937a6f42c325d53effcb1400a3798403fa8b7
Jack <jackfrancis@gmail.com>
2021-08-18 09:25:01 -0700

    use 1.22

--- scripts/cluster-autoscaler-test.sh
+++ scripts/cluster-autoscaler-test.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 CLUSTER_PREFIX="${CLUSTER_PREFIX:-capz-ca}"
-SSH_PUB_KEY_PATH="${SSH_PUB_KEY_PATH:-${HOME}/.ssh/id_rsa.pub}"
+SSH_PUB_KEY_PATH="${SSH_PUB_KEY_PATH:-${HOME}/.ssh/id_ed25519.pub}"
 export CLUSTER_PREFIX_UNIQUE=$CLUSTER_PREFIX-$(uuidgen | cut -c1-5 | awk '{print tolower($0)}')
-export AZURE_LOCATION="${AZURE_LOCATION:-westus2}"
+export AZURE_LOCATION="${AZURE_LOCATION:-eastus2}"
 export CONTROL_PLANE_MACHINE_COUNT="${CONTROL_PLANE_MACHINE_COUNT:-3}"
 export KUBERNETES_VERSION="${KUBERNETES_VERSION:-v1.21.3}"
 export AZURE_SSH_PUBLIC_KEY_B64=$(cat ${SSH_PUB_KEY_PATH} | base64)
-export AZURE_CONTROL_PLANE_MACHINE_TYPE="${AZURE_CONTROL_PLANE_MACHINE_TYPE:-Standard_D2s_v3}"
-export AZURE_NODE_MACHINE_TYPE="${AZURE_NODE_MACHINE_TYPE:-Standard_D2s_v3}"
-export CORES_PER_NODE_VM="${CORES_PER_NODE_VM:-2}"
+export AZURE_CONTROL_PLANE_MACHINE_TYPE="${AZURE_CONTROL_PLANE_MACHINE_TYPE:-Standard_D32s_v3}"
+export AZURE_NODE_MACHINE_TYPE="${AZURE_NODE_MACHINE_TYPE:-Standard_D4s_v3}"
+export CORES_PER_NODE_VM="${CORES_PER_NODE_VM:-4}"
 export WORKER_MACHINE_COUNT="${WORKER_MACHINE_COUNT:-3}"
 export CAPI_NS="${CAPI_NS:-default}"
 export CLUSTER_IDENTITY_NAME="cluster-identity"
 export AZURE_CLUSTER_IDENTITY_SECRET_NAME="cluster-identity-secret"
 export AZURE_CLUSTER_IDENTITY_SECRET_NAMESPACE="default"
-export AUTOSCALER_IMAGE="${AUTOSCALER_IMAGE:-us.gcr.io/k8s-artifacts-prod/autoscaling/cluster-autoscaler:v1.20.0}"
-export CLUSTER_AUTOSCALER_YAML_SPEC="${CLUSTER_AUTOSCALER_YAML_SPEC:-https://gist.githubusercontent.com/jackfrancis/b9b3092ed1add1d35a6b54c8215a5054/raw/b9863e68e68a797853492c028318ddffc1c992b1/cluster-autoscaler.yaml}"
+export AUTOSCALER_IMAGE="${AUTOSCALER_IMAGE:-us.gcr.io/k8s-artifacts-prod/autoscaling/cluster-autoscaler:v1.22.0}"
+export CLUSTER_AUTOSCALER_YAML_SPEC="${CLUSTER_AUTOSCALER_YAML_SPEC:-https://gist.githubusercontent.com/jackfrancis/b9b3092ed1add1d35a6b54c8215a5054/raw/f6d187c3034e4a4af13aedd06a2b027def74d5f5/cluster-autoscaler.yaml}"
 export NUM_WORKLOAD_CLUSTERS="${NUM_WORKLOAD_CLUSTERS:-1}"
 export DESIRED_NEW_NODES="${DESIRED_NEW_NODES:-10}"
 
-#if [ -n "$LARGE_AZURE_SUB_ID" ]; then
-#  export AZURE_SUBSCRIPTION_ID=$LARGE_AZURE_SUB_ID
-#fi
+if [ -n "$LARGE_AZURE_SUB_ID" ]; then
+  export AZURE_SUBSCRIPTION_ID=$LARGE_AZURE_SUB_ID
+fi
 
-#if [ -n "$LARGE_AZURE_SP_ID" ]; then
-#  export AZURE_CLIENT_ID=$LARGE_AZURE_SP_ID
-#fi
+if [ -n "$LARGE_AZURE_SP_ID" ]; then
+  export AZURE_CLIENT_ID=$LARGE_AZURE_SP_ID
+fi
 
-#if [ -n "$LARGE_AZURE_SP_PW" ]; then
-#  export AZURE_CLIENT_SECRET=$LARGE_AZURE_SP_PW
-#fi
+if [ -n "$LARGE_AZURE_SP_PW" ]; then
+  export AZURE_CLIENT_SECRET=$LARGE_AZURE_SP_PW
+fi
 
 if [ -z "$AZURE_SUBSCRIPTION_ID" ]; then
     echo "must provide a AZURE_SUBSCRIPTION_ID env var"
@@ -47,6 +47,7 @@ do
     export CLUSTER_NAME=$CLUSTER_PREFIX_UNIQUE-$i
     export AUTOSCALER_NS=$CLUSTER_NAME
     make envsubst
+    CLUSTER_CREATE_START_TIME=$SECONDS
     # assume kubectl context points to capi mgmt cluster
     hack/tools/bin/envsubst < templates/cluster-template-nat-gateway.yaml | k apply -f -
 
@@ -119,7 +120,8 @@ for ((i=1; i<=NUM_WORKLOAD_CLUSTERS; i++)); do
         fi
         sleep 30
     done
-    echo "No more Pending pods!"
+    CLUSTER_SCALED_OUT_TIME=$(( SECONDS - CLUSTER_CREATE_START_TIME ))
+    echo "No more Pending pods, took $CLUSTER_SCALED_OUT_TIME seconds to schedule all Pending pods!"
     echo "Observed $(k --kubeconfig=$HOME/.kube/$CLUSTER_NAME.kubeconfig get pods | grep 'OutOfcpu' | wc -l | awk '{$1=$1};1') failed pods due to OutOfcpu"
     while true; do
         READY_NODES=$(k --kubeconfig=$HOME/.kube/$CLUSTER_NAME.kubeconfig get nodes | grep -v 'control-plane' | grep '\<Ready' | wc -l | awk '{$1=$1};1')
